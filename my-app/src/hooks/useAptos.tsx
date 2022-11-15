3d9f4dea8677a6c576e8af0921576270cefc4a84
jack-kelly-22 <jacklaxjk@gmail.com>
2022-11-15 14:16:46 -0800

    post merge

--- my-app/src/hooks/useAptos.tsx
+++ my-app/src/hooks/useAptos.tsx
@@ -3,7 +3,7 @@ import { AptosClient, Types } from "aptos";
 const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com";
 const defaultClient = new AptosClient(MAINNET_NODE_URL);
 export const loadTxs = async (address: string, client: AptosClient = defaultClient): Promise<Types.Transaction[]> => {
-  const txs = await client.getAccountTransactions(address)
+  const txs = await client.getAccountTransactions(address,{limit:100})
 
   return txs.reverse();
 
